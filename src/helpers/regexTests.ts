c9bb797d8e9ac857cc7d38539cfa5948d3f92399
Gustavo Ferrara <73325370+gustavoferrara@users.noreply.github.com>
2022-08-01 20:52:48 -0300

    Added regex tests function

---
+++ src/helpers/regexTests.ts
@@ -0,0 +1,11 @@
+const testUrl = (link: string) => {
+  return /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(
+    link,
+  );
+};
+
+export const testEmail = (email: string) => {
+  return /^[^@]+@[^@]{2,}\.[^@]{2,}$/.test(email);
+};
+
+export default testUrl;
